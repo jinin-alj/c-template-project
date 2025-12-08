66ececb93dabbfcb1b5b6790b5abbfcb18f02708
jinin-alj <jaoaljayyous.ieu2024@student.ie.edu>
2025-12-08 20:57:29 +0100

    negative numbers session12

--- src/homework/homework12/bits.c
+++ src/homework/homework12/bits.c
@@ -10,5 +10,14 @@ void print_bits32(int x){
 
 int main(void){
     print_bits32(0xDEADBEEF);
+    print_bits32(1);
+    print_bits32(-1);
     return 0;
 }
+
+// the output is:
+// 00000000000000000000000000000001
+// 11111111111111111111111111111111
+
+// thats because the representation of negative numbers is just the not of the positive number plus one
+// this is two's complement from the notes
