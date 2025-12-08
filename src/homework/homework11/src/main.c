f3accb5603eac0bf134d2eae551f4c77a4a57dc4
jinin-alj <jaoaljayyous.ieu2024@student.ie.edu>
2025-12-08 18:06:35 +0100

    answering questions

--- src/homework/homework11/src/main.c
+++ src/homework/homework11/src/main.c
@@ -6,3 +6,11 @@ int main() {
     printf("Error: %s\n", getErrorMessage(error));
     return 0;
 }
+
+// answer to the questions in the exercise:
+// 1
+// You do not need to handle invalid error codes in your getErrorMessage function. Why?
+// Because ErrorCode is an enum, and the function handles all the defined enum values, any value outside of what I defined is invalid anyway. 
+// 2
+// Can you explain why, despite strerror returning a char *, we do not need to free the returned string?
+// Because the strings returned by strerror() are statically allocated by the C standard library. They should not be freed by the caller.
